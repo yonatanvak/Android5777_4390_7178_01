f4d28934ed90b7c468f50bf7192a2a6bb2246b3d
benaya <hager.benaya@gmail.com>
2016-12-04 16:36:17 +0200

    cbhv

--- app/src/main/java/com/example/android5777_4390_7178_01/model/backend/IDSManager.java
+++ app/src/main/java/com/example/android5777_4390_7178_01/model/backend/IDSManager.java
@@ -1,7 +1,10 @@
 package com.example.android5777_4390_7178_01.model.backend;
 
+import com.example.android5777_4390_7178_01.model.entities.Activities_List;
+import com.example.android5777_4390_7178_01.model.entities.Business;
 import com.example.android5777_4390_7178_01.model.entities.Manager;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -9,6 +12,19 @@ import java.util.ArrayList;
  */
 
 public interface IDSManager {
-final ArrayList<Manager> addManager = new ArrayList<>();
+
+    final ArrayList<Manager> managerList = new ArrayList<Manager>();
+    final ArrayList<Business> businessList = new ArrayList<Business>();
+    final ArrayList<Activities_List> activityList = new ArrayList<Activities_List>();
+
+    void addManager(Manager manager) ;
+    void addBusiness(Business business) throws IOException;
+    void addActivity(Activities_List activities) throws IOException;
+
+    public ArrayList<Manager> getManagerList();
+    public ArrayList<Business> getBusinessList();
+    public ArrayList<Activities_List> getActivityList();
+
+    ///////////////////----seif h--/////////////
 
 }
