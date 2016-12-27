ccffb2d0c6c8eda6f00e230186c3d776555f5d94
benaya <hager.benaya@gmail.com>
2016-12-27 22:38:17 +0200

    dd

--- app/src/main/java/com/example/android5777_4390_7178_01/login.java
+++ app/src/main/java/com/example/android5777_4390_7178_01/login.java
@@ -1,18 +1,23 @@
 package com.example.android5777_4390_7178_01;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.android5777_4390_7178_01.Controller.Registery;
 import com.example.android5777_4390_7178_01.model.Activity_type;
 import com.example.android5777_4390_7178_01.model.datasource.CustomContentProvider;
 
@@ -23,28 +28,64 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-        final SharedPreferences sharedPreferences = getSharedPreferences("TEST",0);
+        final SharedPreferences sharedPreferences = getSharedPreferences("TEST", 0);
         final SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        if (getIntent().getBooleanExtra("Log out", false))
+            finish();
 
         final EditText manegerUser = (EditText) findViewById(R.id.etUserN);
         final EditText passwordUser = (EditText) findViewById(R.id.editTextPassword);
+        final CheckBox CBremember = (CheckBox) findViewById(R.id.cbRemember);
 
-        manegerUser.setText(sharedPreferences.getString("NAME",""));
-        passwordUser.setText(sharedPreferences.getString("PASSWORD",""));
+        if (sharedPreferences.getBoolean("REMEMBER ME", false)) {
+            manegerUser.setText(sharedPreferences.getString("NAME", ""));
+            passwordUser.setText(sharedPreferences.getString("PASSWORD", ""));
+            CBremember.setChecked(true);
+        }
+
+        findViewById(R.id.buttonSign1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, Registery.class);
+                startActivity(intent);
+            }
+        });
+
         findViewById(R.id.buttonLogin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = sharedPreferences.getString("NAME", "no ");
+                String password = sharedPreferences.getString("PASSWORD", "");
 
-
-
-                if (manegerUser.getText().toString().equals("בניה")) {
+                if (manegerUser.getText().toString().equals(name) &&
+                        passwordUser.getText().toString().equals(password)) {
+                    if (CBremember.isChecked()) {
+                        editor.putBoolean("REMEMBER ME", true);
+                        editor.commit();
+                        Log.d("TAG", "remember");
+                    }
+                    else {
+                        editor.putBoolean("REMEMBER ME", false);
+                        editor.commit();
+                        Log.d("TAG", "not remember");
+                    }
                     Intent intent = new Intent(login.this, MainActivity.class);
+                    intent.putExtra("USERNAME", name);
                     startActivity(intent);
                 } else {
-                    Toast.makeText(login.this, "מורדוך סמרטוט !!", Toast.LENGTH_LONG).show();
+                    AlertDialog.Builder problem = new AlertDialog.Builder(login.this);
+                    problem.setTitle("Error");
+                    problem.setMessage("Wrong username or password!");
+                    problem.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    problem.show();
                 }
-                final ContentValues maneger = new ContentValues();
+             /*   final ContentValues maneger = new ContentValues();
                 maneger.put("nameKey", manegerUser.toString());
 
                 new AsyncTask<Void, Void, Void>() {
@@ -53,31 +94,11 @@ public class login extends AppCompatActivity {
                         getContentResolver().insert(CustomContentProvider.MANAGER_CONTENT_URL, maneger);
                         return null;
                     }
-                };
-            }
-        }
+                };*/
+                                                              }
+                                                          }
         );
+    } }
 
-       /* findViewById(R.id.buttonSign).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent sign = new Intent(login.this , Manager.class);
-                startActivity(sign);
-            }
-        });*/
-    }
 
-  /*  public void buttonLogin (View view)
-    {
-        EditText editText = (EditText) findViewById(R.id.editTextUser);
-        String string = editText.getText().toString();
 
-        Toast.makeText(this, string , Toast.LENGTH_SHORT).show();
-    }*/
-
-    public void buttonSign (View view)
-    {
-        Intent sign = new Intent(login.this , MainActivity.class);
-        startActivity(sign);
-    }
-}
